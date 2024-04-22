684e6b8b17f781f47848351695c228f441e2ff22
Muhammad Ashfaq <muhammad.ashfaq@viralsquare.org>
2024-04-22 23:17:24 +0500

    pagination

--- api/movies.ts
+++ api/movies.ts
@@ -4,10 +4,9 @@ const headers = {
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDA2ODg2YTg2MjJkMzVlNTBlOGFiOWEzNGZmNDdhNSIsInN1YiI6IjY2MjYzZmFjMTk3ZGU0MDE3ZDJjNWI0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3DfhFSSEEwGIZEgDdOT8ej4nJiUW_kQq7eEhp-V5WdU",
 }
 
-export const getTopRatedMovies = async () => {
+export const getTopRatedMovies = async ({pageParam}: any) => {
  try {
-  const url =
-   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
+  const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pageParam}`
   const options = {
    method: "GET",
    headers,
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import {ActivityIndicator, FlatList, StyleSheet, Text} from "react-native"
 import {View} from "@/components/Themed"
 import {getTopRatedMovies} from "@/api/movies"
-import {useQuery} from "@tanstack/react-query"
+import {useInfiniteQuery} from "@tanstack/react-query"
 import MovieListItem from "@/components/MovieListItem"
 
 export default function TabOneScreen() {
- const {data, isLoading, error} = useQuery({
+ const {data, isLoading, error, fetchNextPage} = useInfiniteQuery({
   queryKey: ["movies"],
+  initialPageParam: 1,
   queryFn: getTopRatedMovies,
+  getNextPageParam: (lastPage, pages) => pages.length + 1,
  })
 
  const renderMovie = ({item, index}: any) => {
@@ -22,14 +24,19 @@ export default function TabOneScreen() {
   return <Text>{error.message}</Text>
  }
 
+ const movies = data?.pages?.flat()
  return (
   <View style={styles.container}>
    <FlatList
-    data={data}
+    data={movies}
     numColumns={2}
     contentContainerStyle={{gap: 5}}
     columnWrapperStyle={{gap: 5}}
     renderItem={renderMovie}
+    onEndReached={() => {
+     console.log("[onEndReached Called]")
+     fetchNextPage()
+    }}
    />
   </View>
  )
