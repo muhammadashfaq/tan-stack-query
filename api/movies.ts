57eb54217f4c155b4ff866b0adc208ef50af68d0
Muhammad Ashfaq <muhammad.ashfaq@viralsquare.org>
2024-04-22 18:49:18 +0500

    Fetching movie detail by TanStack Query

--- api/movies.ts
+++ api/movies.ts
@@ -1,19 +1,35 @@
+const headers = {
+ accept: "application/json",
+ Authorization:
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDA2ODg2YTg2MjJkMzVlNTBlOGFiOWEzNGZmNDdhNSIsInN1YiI6IjY2MjYzZmFjMTk3ZGU0MDE3ZDJjNWI0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3DfhFSSEEwGIZEgDdOT8ej4nJiUW_kQq7eEhp-V5WdU",
+}
+
 export const getTopRatedMovies = async () => {
  try {
   const url =
    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
   const options = {
    method: "GET",
-   headers: {
-    accept: "application/json",
-    Authorization:
-     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDA2ODg2YTg2MjJkMzVlNTBlOGFiOWEzNGZmNDdhNSIsInN1YiI6IjY2MjYzZmFjMTk3ZGU0MDE3ZDJjNWI0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3DfhFSSEEwGIZEgDdOT8ej4nJiUW_kQq7eEhp-V5WdU",
-   },
+   headers,
   }
-
   const respsonse = await fetch(url, options)
   const movies = await respsonse.json()
   return movies.results
+ } catch (err) {
+  console.log("[err]", err)
+ }
+}
+
+export const getMovieDetail = async (id: number) => {
+ try {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+  const options = {
+   method: "GET",
+   headers,
+  }
+  const respsonse = await fetch(url, options)
+  const movies = await respsonse.json()
+  return movies
  } catch (err) {
   console.log("[err]", err)
  }
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,14 +1,30 @@
+import {getMovieDetail} from "@/api/movies"
+import {useQuery} from "@tanstack/react-query"
 import {useLocalSearchParams} from "expo-router"
 import * as React from "react"
-import {Text, View, StyleSheet} from "react-native"
+import {Text, View, StyleSheet, ActivityIndicator} from "react-native"
 
 interface MovieDetailsProps {}
 
 const MovieDetails = (props: MovieDetailsProps) => {
  const {id} = useLocalSearchParams()
+
+ const {data, isLoading, error} = useQuery({
+  queryKey: ["movies", id],
+  queryFn: () => getMovieDetail(id),
+ })
+
+ if (isLoading) {
+  return <ActivityIndicator />
+ }
+
+ if (error) {
+  return <Text>{error.message}</Text>
+ }
+
  return (
   <View style={styles.container}>
-   <Text>MovieDetails: {id}</Text>
+   <Text>MovieDetails: {data.title}</Text>
   </View>
  )
 }
